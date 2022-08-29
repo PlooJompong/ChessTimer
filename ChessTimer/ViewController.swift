0ea21f502191f073f7cc789c8f40ae12b40383db
ponjom2 <ponjom2@hotmail.com>
2022-08-29 13:05:22 +0200

    Initial Commit

---
+++ ChessTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessTimer
+//
+//  Created by Ploo Jompong on 2022-08-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
